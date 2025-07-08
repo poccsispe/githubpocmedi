9c90b073442475a4fe31bd8bddbdf79e7f7f5978
mediDEU <mehdivasef@gmail.com>
2025-07-08 16:00:55 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,13 @@
+
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.0"
+    github = {
+      source = "integrations/github"
     }
+ 
   }
 }
+
 
 provider "azurerm" {
   features {}
